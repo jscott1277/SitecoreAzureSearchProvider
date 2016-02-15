0c9c812072b34a3fc4af6773d4e3961f90ac6c34
jscott1277 <jscott1277@hotmail.com>
2016-02-15 11:36:07 -0600

    Added excluded item

---
+++ Slalom.ContentSearch.AzureProvider/Queries/NotEqualQuery.cs
@@ -0,0 +1,25 @@
+﻿using Lucene.Net.Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slalom.ContentSearch.AzureProvider.Queries
+{
+    public class NotEqualQuery : Query
+    {
+        public string FieldName { get; private set; }
+        public string FieldValue { get; private set; }
+
+        public NotEqualQuery(string fieldName, string fieldValue)
+        {
+            FieldName = fieldName;
+            FieldValue = fieldValue;
+        }
+        public override string ToString(string field)
+        {
+            return string.Format("+(-{0}:\"\"{1})", FieldName, FieldValue);
+        }
+    }
+}
