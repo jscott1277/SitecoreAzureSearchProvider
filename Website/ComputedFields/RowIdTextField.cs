90596d8b3ed9b904ff7c8b4df41a69117b8629ae
jscott1277 <jscott1277@hotmail.com>
2016-03-14 20:46:07 -0400

    Added computed test field test

---
+++ Website/ComputedFields/RowIdTextField.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.ComputedFields;
+using Sitecore.Data.Items;
+using System;
+
+namespace Website.ComputedFields
+{
+    public class RowIdTextField : IComputedIndexField
+    {
+        public virtual string FieldName { get; set; }
+
+        public virtual string ReturnType { get; set; }
+
+        public object ComputeFieldValue(IIndexable indexable)
+        {
+            var item = (Item)(indexable as SitecoreIndexableItem);
+
+            if (item == null)
+                return null;
+
+            return Guid.NewGuid();
+        }
+    }
+}
